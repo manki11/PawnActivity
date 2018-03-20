9a2889ec91fe53e1b5faf5401cd6607847d0048f
Mankirat Singh <mankirat1998@gmail.com>
2018-03-20 21:26:05 +0530

    feat: step 3 completed

--- js/activity.js
+++ js/activity.js
@@ -1,15 +1,28 @@
-define(["sugar-web/activity/activity", "sugar-web/env"], function (activity, env) {
-
-    // Manipulate the DOM only when it is ready.
+define(["sugar-web/activity/activity", "sugar-web/env", "sugar-web/graphics/icon"], function (activity, env, icon) {    // Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
 
 		// Initialize the activity.
 		activity.setup();
 
+        var currentenv;
         env.getEnvironment(function(err, environment) {
+            currentenv = environment;
             document.getElementById("user").innerHTML = "<h1>"+"Hello"+" "+environment.user.name+" !</h1>";
         });
 
-	});
+        // Handle click on add
+        document.getElementById("add-button").addEventListener('click', function (event) {
+            var pawn = document.createElement("div");
+            pawn.className = "pawn";
+
+            document.getElementById("pawns").appendChild(pawn);
+            icon.colorize(pawn, currentenv.user.colorvalue);
+            console.log(currentenv.user.colorvalue);
+
+            document.getElementById("user").innerHTML = "<h1>"+currentenv.user.name+" played !</h1>";
+        });
+
+
+    });
 
 });
